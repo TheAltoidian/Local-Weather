f73cc82155a79235150ef77e03cd9557b012b297
Cullen <cullenjcasey@gmail.com>
2023-09-10 18:40:29 -0700

    made it look pretty

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 let addressForm = document.querySelector("#input-form");
 
-
-
+// capitalizes first letter of city when searched
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 //update default text with previously searched weather
 function updatePage() {
     document.getElementById("displayedCity").textContent = localStorage.getItem("recentCity");
-    document.getElementById("displayedWeather").textContent = localStorage.getItem("recentWeather") + "°F";
+    document.getElementById("displayedWeather").textContent = localStorage.getItem("recentWeather") + "°F/" + Math.round((localStorage.getItem("recentWeather") - 32) * 5 / 9) + "°C";
     document.getElementById("displayedIcon").src = weatherImage(pastCities[0].icon);
     document.getElementById("pastCity1").textContent = pastCities[1].city;
-    document.getElementById("pastWeather1").textContent = pastCities[1].weather + "°F";
+    document.getElementById("pastWeather1").textContent = pastCities[1].weather + "°F/" + Math.round((pastCities[1].weather - 32) * 5 / 9) + "°C";
     document.getElementById("pastIcon1").src = weatherImage(pastCities[1].icon);
     document.getElementById("pastCity2").textContent = pastCities[2].city;
-    document.getElementById("pastWeather2").textContent = pastCities[2].weather + "°F";
+    document.getElementById("pastWeather2").textContent = pastCities[2].weather + "°F/" + Math.round((pastCities[2].weather - 32) * 5 / 9) + "°C";
     document.getElementById("pastIcon2").src = weatherImage(pastCities[2].icon);
     document.getElementById("pastCity3").textContent = pastCities[3].city;
-    document.getElementById("pastWeather3").textContent = pastCities[3].weather + "°F";
+    document.getElementById("pastWeather3").textContent = pastCities[3].weather + "°F/" + Math.round((pastCities[3].weather - 32) * 5 / 9) + "°C";
     document.getElementById("pastIcon3").src = weatherImage(pastCities[3].icon);
     document.getElementById("pastCity4").textContent = pastCities[4].city;
-    document.getElementById("pastWeather4").textContent = pastCities[4].weather + "°F";
+    document.getElementById("pastWeather4").textContent = pastCities[4].weather + "°F/" + Math.round((pastCities[4].weather - 32) * 5 / 9) + "°C";
     document.getElementById("pastIcon4").src = weatherImage(pastCities[4].icon);
 }
 
@@ -32,7 +34,7 @@ function weatherImage(code) {
         return ('./icons/fog.png')
     } else if ([51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82].includes(code)) {
         return ('./icons/rain.png')
-    } else if ([71, 73, 75, 77, 85, 86].includes(code)){
+    } else if ([71, 73, 75, 77, 85, 86].includes(code)) {
         return ('./icons/snow.png')
     } else if ([95, 96, 98].includes(code)) {
         return ('./icons/lightning.png')
@@ -49,8 +51,9 @@ function submitInfo(event) {
 
     //get city from page, turn it into a url
     let city = document.getElementById("city").value;
-    let unspacedCity = city.replace(/ /g, "+");
-    search = 'http://api.openweathermap.org/geo/1.0/direct?q=' + unspacedCity + '&limit=1&appid=20218664feca793b07da2ba8243b7197';
+    city = capitalizeFirstLetter(city)
+    document.getElementById('city').value = ''
+    search = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=20218664feca793b07da2ba8243b7197';
 
     //turn city into coordinates, pass coordinates to weather function
     fetch(search)
@@ -98,4 +101,3 @@ if (localStorage.getItem("pastCities")) {
 }
 if (localStorage.getItem("recentCity") && localStorage.getItem("recentWeather") && localStorage.getItem("recentIcon")) { updatePage() }
 addressForm.addEventListener("submit", submitInfo)
-//updates list of previously viewed cities and weathers 
